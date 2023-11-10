3ed96f9522ea07becd87d21fc1e4b1b23eece953
manoharreddy0077 <smanoharreddy1739@gmail.com>
2023-11-10 14:45:02 +0530

    addingandremovingfromcart

--- src/Components/Menu/Cart.jsx
+++ src/Components/Menu/Cart.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cart.css'
-const Cart = () => {
+const Cart = ({cartItems}) => {
+  const calculateTotalPrice=()=>{
+      return cartItems.reduce((total,item)=>total+item.Price,0);
+  }
+  
+  console.log(cartItems)
   return (
     <div>
-      {/* <h1>this is cart</h1> */}
-      <button className='cartbox'>cart</button>
+      <h2>Your Cart</h2>
+      
+      <div className="cartbox">
+        {cartItems.map((item,index)=>(
+            <div  key={index} className="cart-item">
+              <p>Item : {item.Item}</p>
+              <p>Price : {item.Price}</p>
+            </div>
+        ))}
+      </div>
+          <h3>Total Price : {calculateTotalPrice()}</h3>
+          <button>Proceed to Pay</button>
     </div>
   )
 }
--- src/Components/Menu/MenuList.jsx
+++ src/Components/Menu/MenuList.jsx
@@ -1,29 +1,84 @@
+// its not being displayed in cartbox when props are passed using these jsx file 
 import React from 'react'
 import './MenuList.css'
 import { useState } from 'react'
 import Cart from './Cart'
 
 const MenuList = () => {
-  const [quan,setQuan]=useState(0);
+
+  // const [quan,setQuan]=useState(0);
   const [Canteen,setCanteen]=useState('');
   const[menuData,setMenuData]=useState([]);
-  const handleIncrement=()=>{
-    if(quan>=0 && quan<=20)
+  const [cartItems,setcartItems]=useState([]);
+
+  const [quantities,setQuantities]=useState({});
+
+const handleQuantityChange =(itemId,action,Item,Price)=>{
+    if(action==='+')
     {
-      setQuan(quan+1)
-    }else{
-      alert('Cant Perform the Operation')
+          if(quantities[itemId]>=0 && quantities[itemId]<20)
+          {
+              setQuantities((prevQuantities)=>({
+                ...prevQuantities,
+                [itemId]:(prevQuantities[itemId] || 0)+1,
+              }))
+              alert(`item added to cart : ${Item}, Price Rs.${Price}`);
+              setcartItems((prevItems)=>[...prevItems,{Item: Item,Price:Price}])
+          }
     }
+    else if(action==='-')
+      {
+        //1st version will remove all the items cuz we have filtered out using Item name
+        // if(quantities[itemId]>0)
+        // {
+        //   setQuantities((prevQuantities)=>({
+        //     ...prevQuantities,
+        //     [itemId]:prevQuantities[itemId]-1,
+        //   }))
+
+        //   const existingCartItem=cartItems.find((cartItem)=>cartItem.Item===Item);
+        //   // console.log("existing ",existingCartItem);
+
+        //   if(existingCartItem && existingCartItem.quantity >1){
+        //     setcartItems((prevItems)=>
+        //         prevItems.map((cartItem)=>
+        //                   cartItem.Item===Item ? { ...cartItem, quantity: cartItem.quantity - 1 } : cartItem              
+        //           )
+        //     );
+        //   }
+        //   else {
+        //     // If the quantity is 1 or the item doesn't exist in the cart, remove it
+        //     setcartItems((prevItems) =>
+        //       prevItems.filter((cartItem) => cartItem.Item !== Item)
+        //     );
+        if (quantities[itemId] > 0) {
+          setQuantities((prevQuantities) => ({
+            ...prevQuantities,
+            [itemId]: prevQuantities[itemId] - 1,
+          }));
     
-  }
-  const handleDecrement=()=>{
-    if(quan>0)
-    setQuan(quan-1)
+          // Update quantity in the cart
+          setcartItems((prevItems) =>
+            prevItems.map((cartItem) =>
+              cartItem.Item === Item
+                ? { ...cartItem, quantity: quantities[itemId] }
+                : cartItem
+            )
+          );
+    
+          // If the quantity is 1, remove the item from the cart
+          if (quantities[itemId] === 1) {
+            setcartItems((prevItems) =>
+              prevItems.filter((cartItem) => cartItem.Item !== Item)
+            );
+
+        
+        }
+      }
+    }
   }
   const handleClick=async(e)=>{
     try{
-    // e.preventDefault();
-    // console.log(e);
     setCanteen(e);
     const  response= await fetch('http://localhost:3001/api/menu',{
       method:'POST',
@@ -31,15 +86,23 @@ const MenuList = () => {
         'Content-Type':'application/json',
       },body:JSON.stringify({canteen_name:e})
     })
-    // alert("displayed");
     if(response.ok){
       const data=await response.json();
       setMenuData(data);
-      // console.log(menuData);
+
+
+      //initialse quantities state for each menu item
+
+      const initialQuantities={};
+      data.forEach(item => {
+          initialQuantities[item._id]=0;
+      });
+
+      setQuantities(initialQuantities);
     }else{
-      console.log(menuData);
+      // console.log(menuData);
       console.log('request failed with status ',response.status);
-      console.log(response)
+      // console.log(response)
     }
   }catch(error){
     console.log('An error Occured',error);
@@ -48,15 +111,13 @@ const MenuList = () => {
 
 //render menu items
 const renderMenuItems=()=>{
- 
-  
   return( 
    <table>
     <thead className='heading'>
       <tr>
-      <th>Item&nbsp;&nbsp;</th>
+      <th>Item&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
       <th>price&nbsp;&nbsp;&nbsp;</th>
-      <th>Quantity Left&nbsp;&nbsp;</th>
+      {/* <th>Quantity Left&nbsp;&nbsp;</th> */}
       <th>Select Your Food</th>
     </tr>
     </thead>
@@ -68,21 +129,18 @@ const renderMenuItems=()=>{
         <tr key={item._id} className='eachitem'>
           <td>{item.Item}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
           <td>Rs.{item.Price}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-          <td>{item.Quantity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
-          <button onClick={() => handleIncrement()} className='orderbtn'>+</button>
-                  <p className='orderquan'>{quan}</p>
-          <button onClick={() => handleDecrement()} className='orderbtn'>-</button>
+          {/* <td>{item.Quantity}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td> */}
+
+          <button  className='orderbtn' onClick={()=>handleQuantityChange(item._id,'+',item.Item,item.Price)}>+</button>
+                  {/* <p className='orderquan'>{quantities[item._id]}</p> */}
+          <button className='orderbtn' onClick={()=>handleQuantityChange(item._id,'-',item.Item,item.Price)}>-</button>
         </tr> 
       ))}
     </div>
-      
-
-
     </tbody>
     </table>
     )
 }
-
   return (
     <div className='allcanteens'>
       <div className="canteensdisplay">
@@ -111,11 +169,13 @@ const renderMenuItems=()=>{
         {menuData.length>0 ? renderMenuItems() : <p>No Menu Items</p>}
       </div>
       <div className="cart">
-        <Cart/>
+        <Cart cartItems={cartItems}/>
       </div>
       </div>
     </div>
   );
 };
 
-export default MenuList
+export default MenuList;
+
+
